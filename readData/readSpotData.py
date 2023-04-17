adb0b6ca3caf1845232c952bd3126da730fffe06
hellevillmoneshaug <hellevha@stud.ntnu.no>
2023-04-17 12:02:33 +0200

    spot fixes

--- readData/readSpotData.py
+++ readData/readSpotData.py
@@ -7,3 +7,21 @@ def initialize_spot_sets():
     fob_spot_ids = []
 
     return spot_port_ids, des_spot_ids, fob_spot_ids
+
+def read_spot_des_contracts(data, spot_port_ids, des_spot_ids):
+
+    for spot_des_contract in data['spotDesRequests']:
+        des_spot_ids.append(spot_des_contract['id'])
+        if spot_des_contract['portId'] not in spot_port_ids:
+            spot_port_ids.append(spot_des_contract['portId'])
+        
+        0
+
+    return 0
+
+def read_spot_fob_contracts(data, spot_port_ids, fob_spot_ids):
+
+    for spot_fob_contract in data['spotFobRequests']:
+        0
+
+    return 0 
