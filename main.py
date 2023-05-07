1b85763dae6bb858d2b448bedf239376b1f4b686
hellevillmoneshaug <hellevha@stud.ntnu.no>
2023-05-07 10:36:29 +0200

    new data

--- main.py
+++ main.py
@@ -143,14 +143,14 @@ Call whatever functions you'll like below here
 """
 
 # An example for how to run the code 
-group = 'N-1L-A'
+group = 'N-1L-B'
 filename = 'N-1L-16U-12F-23V-120D'
-group1 = 'A-2L-B'
-filename1 = 'A-2L-6U-20F-15V-120D'
+group1 = 'A-2L-D'
+filename1 = 'A-2L-6U-8F-15V-120D'
 runtime = 60*60*4
 modelType = BASIC_MODEL
 
-run_one_instance(group, filename, runtime, modelType)
+run_one_instance(group1, filename1, runtime, modelType)
 #test_init_model(group, filename, modelType)
 #run_all_model_types(group1, filename1, runtime)
 #run_all_model_types_for_group(group1, runtime)
