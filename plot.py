d50971f23c1603d9e3d5022fb5b5e2fc1e2fd83b
Sigrid Solum <sigrihso@ntnu.no>
2023-05-12 09:59:29 +0200

    new testdata, logfiles and jsonfiles

--- plot.py
+++ plot.py
@@ -22,15 +22,15 @@ File for plotting different stuffz
 
 
 ## Example of how to plot a gant chart ##
-group1 = 'A-2L-B'
-datafile1 = 'A-2L-6U-20F-15V-120D'
-logFile1 = f'jsonFiles/A-2L-B/A-2L-6U-20F-15V-120D/05-04-2023, 18-39.json'
-spotGroup = 'spotTests'
-spotDataFile = 'N-1L-10U-6F-23V-60D'
-spotLogFile = f'jsonFiles/spotTests/A-2L-6U-17F-15V-120D/05-04-2023, 18-36.json'
+group1 = 'N-1L-D'
+datafile1 = 'N-1L-14U-14F-23V-120D'
+logFile1 = f'jsonFiles/N-1L-D/N-1L-14U-14F-23V-120D/05-11-2023, 14-06.json'
+#spotGroup = 'spotTests'
+#spotDataFile = 'N-1L-10U-6F-23V-60D'
+#spotLogFile = f'jsonFiles/spotTests/A-2L-6U-17F-15V-120D/05-04-2023, 18-36.json'
 
 solutionData = read_solved_json_file(logFile1)
-contract_gant_chart(solutionData, group1, datafile1, UNLOADING) #Can plot both loading and unloading
+contract_gant_chart(solutionData, group1, datafile1, LOADING) #Can plot both loading and unloading
 plot_inventory_levels(solutionData, group1, datafile1)
 plot_produced_lng(solutionData, group1, datafile1)
 
